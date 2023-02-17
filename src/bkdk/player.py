bab54e6fb06706bd77d4d7203dc99edab63107d6
Gary Benson <gary@gbenson.net>
2023-02-17 15:48:47 +0000

    Handle second move

--- src/bkdk/player.py
+++ src/bkdk/player.py
@@ -2,11 +2,27 @@ class Player:
     def __init__(self, activate):
         self._activate = activate
 
+    _EMPTY_CHOICE = tuple(0 for _ in range(25))
+
+    def _choice_states_for(self, board):
+        for shape in board.choices:
+            if shape is None:
+                yield None
+            else:
+                yield board.can_place(shape)
+
     def _collect_state_from(self, board):
+        choice_states = tuple(self._choice_states_for(board))
+        if not any(cs is True for cs in choice_states):
+            return
+
         # XXX might this method be faster with s/int/float/ ?
         state = [int(cell.is_set) for cell in board.cells]
-        for shape in board.choices:
-            values = [0, int(board.can_place(shape)) * 2 - 1]
+        for shape, can_place in zip(board.choices, choice_states):
+            if shape is None:
+                state.extend(self._EMPTY_CHOICE)
+                continue
+            values = [0, int(can_place) * 2 - 1]
             state.extend(values[i] for i in shape.cells)
         return state
 
@@ -25,6 +41,8 @@ class Player:
 
     def next_move(self, board):
         inputs = self._collect_state_from(board)
+        if inputs is None:
+            return  # No moves!
         # print(self._state_to_str(inputs))
         outputs = self._activate(inputs)
         ranked_outputs = list(sorted(
@@ -36,7 +54,8 @@ class Player:
             cell, choice = divmod(output, 3)
             rowcol = divmod(cell, 9)
             shape = board.choices[choice]
-            is_legal = board.can_place_at(rowcol, shape)
+            is_legal = (shape is not None
+                        and board.can_place_at(rowcol, shape))
             print(f"output {output} => shape {choice} at {rowcol}?"
                   f" {is_legal and 'YES!' or 'no'}")
             if not is_legal:
