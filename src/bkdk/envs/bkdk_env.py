599e61d62f956bbf930bc7297097d9d98608002c
Gary Benson <gary@gbenson.net>
2023-02-20 15:43:33 +0000

    Implement env.step()

--- src/bkdk/envs/bkdk_env.py
+++ src/bkdk/envs/bkdk_env.py
@@ -9,23 +9,21 @@ from ..board import Board
 class BkdkEnv(gym.Env):
     def __init__(self, render_mode=None):
         # XXX fetch these from somewhere... bkdk.Board?
-        board_size = 9
-        shape_size = 5
+        self.board_size = 9
+        self.shape_size = 5
         num_choices = 3
 
         self.observation_space = spaces.Dict({
             "board": spaces.Box(
                 low=0, high=1, dtype=np.uint8,
-                shape=(board_size, board_size)),
+                shape=(self.board_size, self.board_size)),
             "choices": spaces.Box(
                 low=0, high=1, dtype=np.uint8,
-                shape=(num_choices, shape_size, shape_size)),
+                shape=(num_choices, self.shape_size, self.shape_size)),
             })
 
-        """The action is encoded as an integer,
-        `column + row*BOARD_SIZE + choice_index*BOARD_SIZE**2`.
-        """
-        self.action_space = spaces.Discrete(board_size**2 * num_choices)
+        # An integer, encoded as per self.step.__doc__
+        self.action_space = spaces.Discrete(self.board_size**2 * num_choices)
 
     @property
     def _observation(self):
@@ -33,8 +31,12 @@ class BkdkEnv(gym.Env):
             "board": np.asarray([[int(cell.is_set) for cell in row]
                                  for row in self._board.rows],
                                 dtype=np.uint8),
-            "choices": np.asarray([shape._rows
-                                   for shape in self._board.choices],
+            "choices": np.asarray([
+                shape is None
+                and tuple(tuple(0 for _ in range(self.shape_size))
+                          for _ in range(self.shape_size))
+                or shape._rows
+                for shape in self._board.choices],
                                   dtype=np.uint8),
         }
 
@@ -49,3 +51,22 @@ class BkdkEnv(gym.Env):
         super().reset(seed=seed)
         self._board = Board(random_number_generator=self.np_random)
         return self._observation, self._info
+
+    def step(self, action):
+        """Run one move of the game.
+
+        :param action: may be a tuple of (choice, row, column),
+        or the same encoded as as an integer via:
+        `column + row*BOARD_SIZE + choice_index*BOARD_SIZE**2`.
+        """
+        if isinstance(action, type(0)):
+            choice_row, column = divmod(action, self.board_size)
+            choice, row = divmod(choice_row, self.board_size)
+        else:
+            choice, row, column = action
+
+        reward = self._board.one_move(choice, (row, column))
+        terminated = not any(self._board.can_place(shape)
+                             for shape in self._board.choices)
+
+        return self._observation, reward, terminated, False, self._info
--- tests/test_env.py
+++ tests/test_env.py
@@ -84,3 +84,45 @@ def test_initial_choices(initial_choices):
                  ZEROS, ZEROS),
             ),
             dtype=np.uint8))
+
+
+@pytest.mark.parametrize(
+    "action",
+    ((2, 3, 4),  # shape 2, row 3, column 4
+     193,        # ditto, but flattened into an int
+     ))
+@pytest.mark.filterwarnings(f"ignore:{_GYMNASIUM_269}")
+def test_step(env, action):
+    """env.step() does what it should."""
+    observation, info = env.reset(seed=23)
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    ZEROS = tuple(0 for _ in range(9))
+    assert np.array_equal(
+        observation["board"],
+        np.array((ZEROS, ZEROS, ZEROS,
+                  (0, 0, 0,  0, 0, 0,  1, 0, 0),
+                  (0, 0, 0,  0, 1, 1,  1, 0, 0),
+                  ZEROS, ZEROS, ZEROS, ZEROS), dtype=np.uint8))
+
+    ZEROS = tuple(0 for _ in range(5))
+    assert np.array_equal(
+        observation["choices"],
+        np.array(
+            (
+                # shape 0: code="xx"
+                (ZEROS, ZEROS,
+                 (0, 1, 1, 0, 0),
+                 ZEROS, ZEROS),
+
+                # shape 1: code="xx_x-_xx"
+                (ZEROS,
+                 (0, 1, 1, 0, 0),
+                 (0, 1, 0, 0, 0),
+                 (0, 1, 1, 0, 0),
+                 ZEROS),
+
+                # shape 2: used up
+                tuple(ZEROS for _ in range(5)),
+            ),
+            dtype=np.uint8))
