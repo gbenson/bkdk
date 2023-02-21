cd967869aefc0a45536de0b26ea5a38c3ff4fac0
Gary Benson <gary@gbenson.net>
2023-02-21 00:24:41 +0000

    Silence Gymnasium's env checker warnings

--- src/bkdk/__init__.py
+++ src/bkdk/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+
 from gymnasium.envs.registration import register as _gym_env_register
 
 from .board import Board
@@ -8,3 +10,9 @@ _gym_env_register(
     id="bkdk/BKDK-v0",
     entry_point="bkdk.envs:BkdkEnv",
 )
+
+# Gymnasium's passive environment checker issues warnings about our
+# observation spaces having unconventional shapes and bounds, twice
+# for every genome we evaluate.  There's a Gymnasium issue, #269:
+# https://github.com/Farama-Foundation/Gymnasium/issues/269
+warnings.filterwarnings("ignore", message=r".*Box observation space.*")
