32fcd020c7a0866ec60194c437a537e6573aeba0
Gary Benson <gary@gbenson.net>
2023-02-27 00:03:10 +0000

    [BRANCH] Introduce Board._can_place_at

--- src/bkdk/board.py
+++ src/bkdk/board.py
@@ -73,6 +73,9 @@ class Board:
         top-left corner at rowcol, False otherwise."""
         if any(d < 0 for d in rowcol):
             return False
+        return self._can_place_at(rowcol, shape)
+
+    def _can_place_at(self, rowcol, shape):
         if any(d + s > 9 for d, s in zip(rowcol, shape.size)):
             return False
         return not any(cell.is_set
