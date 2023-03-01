023d01f4f9a74413526aea8f1cbc9ee0bf287c12
Gary Benson <gary@gbenson.net>
2023-03-01 23:39:22 +0000

    Start version 0.0.5

--- src/bkdk/__init__.py
+++ src/bkdk/__init__.py
@@ -4,7 +4,7 @@ from gymnasium.envs.registration import register as _gym_env_register
 
 from .ts.core import TinyScreen
 
-__version__ = "0.0.4"
+__version__ = "0.0.5.dev"
 
 _gym_env_register(
     id="bkdk/BKDK-v0",
