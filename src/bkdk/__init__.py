2118dadeba92bad00699cf4cabd54dad1c18242e
Gary Benson <gary@gbenson.net>
2023-03-01 22:17:58 +0000

    Rename bkdk.gym as bkdk.env

--- src/bkdk/__init__.py
+++ src/bkdk/__init__.py
@@ -8,7 +8,7 @@ __version__ = "0.0.4.dev"
 
 _gym_env_register(
     id="bkdk/BKDK-v0",
-    entry_point="bkdk.gym:Env",
+    entry_point="bkdk.env:Env",
 )
 
 # Gymnasium's passive environment checker issues warnings about our
