008ca0a0747e4041c89f8f25d31bbc3b2e9686e8
Gary Benson <gary@gbenson.net>
2023-05-10 23:00:56 +0100

    Introduce ChoiceDecoder

--- src/bkdk/screenshot.py
+++ src/bkdk/screenshot.py
@@ -22,6 +22,19 @@ class ScreenshotDecoder:
                              self.height // 2 - yoffset),
                             cellsize)
 
+    @property
+    def choices(self):
+        xcen = self.width // 2
+        xcen_plusminus = self.width * 5 // 16
+        ycen = self.board.cen_xy[1] + self.width * 105 // 144
+        cellsize = self.width // 18
+        return tuple(
+            ChoiceDecoder(self,
+                          ((xcen + xcen_plusminus * (i - 1),
+                            ycen)),
+                          cellsize)
+            for i in range(3))
+
 
 class BoardDecoder:
     def __init__(self, img, cen_xy, cellsize):
@@ -40,6 +53,44 @@ class BoardDecoder:
     def tolist(self):
         return np.asarray([cell.is_set for cell in self.cells],
                           dtype=np.uint8).reshape((9, 9)).tolist()
+
+
+class ChoiceDecoder:
+    def __init__(self, img, cen_xy, cellsize):
+        self.img = img
+        self.cen_xy = cen_xy
+        self.cellsize = cellsize
+        for self.num_rows in 4, 5:
+            for self.num_cols in 4, 5:
+                try:
+                    list(cell.is_set for cell in self.cells)
+                    return
+                except CellDecodingError:
+                    pass
+
+    @property
+    def _shape(self):
+        return self.num_rows, self.num_cols
+
+    @property
+    def cells(self):
+        for row in range(self.num_rows):
+            yoff = (row * 2 - (self.num_rows - 1)) * self.cellsize // 2
+            ycen = self.cen_xy[1] + yoff
+            for col in range(self.num_cols):
+                xoff = (col * 2 - (self.num_cols - 1)) * self.cellsize // 2
+                xcen = self.cen_xy[0] + xoff
+                yield CellDecoder(self.img, (xcen, ycen), self.cellsize)
+
+    def tolist(self):
+        result = np.asarray([cell.is_set for cell in self.cells],
+                            dtype=np.uint8).reshape(self._shape)
+        if self.num_cols < 5:
+            result = np.hstack((result, np.zeros((self.num_rows, 1),
+                                                 dtype=result.dtype)))
+        if self.num_rows < 5:
+            result = np.vstack((result, np.zeros(5, dtype=result.dtype)))
+        return result.tolist()
 
 
 class CellDecoder:
@@ -61,5 +112,11 @@ class CellDecoder:
         num_pixels = img.width * img.height
         colors = img.getcolors(num_pixels)
         max_count, color = max(colors)
-        assert max_count * 100 > num_pixels * 99
+        confidence = 100 * max_count // num_pixels
+        if confidence < 99:
+            raise CellDecodingError(f"{confidence}% at {self.rect}")
         return 0 in color
+
+
+class CellDecodingError(ValueError):
+    pass
--- tests/test_screenshot.py
+++ tests/test_screenshot.py
@@ -16,6 +16,11 @@ def test_screenshot():
     return load_test_screenshot("20230430-103701.jpg")
 
 
+@pytest.fixture
+def test_screenshot2():
+    return load_test_screenshot("20230430-105149.jpg")
+
+
 def test_screenshots_subclass_PIL_Image(test_screenshot):
     """Screenshots subclass PIL.Image."""
     assert isinstance(test_screenshot, Image.Image)
@@ -53,4 +58,61 @@ def test_screenshot_board_tolist(test_screenshot):
         [0, 1, 0,  1, 1, 1,  0, 0, 0],
         [1, 0, 0,  1, 1, 1,  0, 0, 0],
         [1, 0, 0,  0, 0, 0,  0, 0, 0],
+    ]
+
+
+def test_screenshot_choices(test_screenshot):
+    """Screenshots have 3 choices"""
+    assert len(test_screenshot.choices) == 3
+
+
+def test_screenshot_centred_choice_cells(test_screenshot):
+    """Choices with centred cells may be decoded."""
+    choice = test_screenshot.choices[0]
+    assert choice._shape == (5, 5)
+    assert choice.tolist() == [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0],
+    ]
+
+
+def test_screenshot_hoffset_choice_cells(test_screenshot):
+    """Choices with horizontally offset cells may be decoded."""
+    choice = test_screenshot.choices[2]
+    assert choice._shape == (5, 4)
+    assert choice.tolist() == [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+
+
+def test_screenshot_voffset_choice_cells(test_screenshot2):
+    """Choices with vertically offset cells may be decoded."""
+    choice = test_screenshot2.choices[0]
+    assert choice._shape == (4, 5)
+    assert choice.tolist() == [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+
+
+def test_screenshot_fullskew_choice_cells(test_screenshot2):
+    """Choices with fully offset cells may be decoded."""
+    choice = test_screenshot2.choices[1]
+    assert choice._shape == (4, 4)
+    assert choice.tolist() == [
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ]
