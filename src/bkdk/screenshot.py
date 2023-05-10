9ddd7b94e41246a0e4e1ef3da6ceaeb4eed51373
Gary Benson <gary@gbenson.net>
2023-05-11 00:23:46 +0100

    Improve alignment

--- src/bkdk/screenshot.py
+++ src/bkdk/screenshot.py
@@ -14,27 +14,27 @@ def Screenshot(*args, **kwargs):
 class ScreenshotDecoder:
     @property
     def board(self):
-        cellsize = min(self.size) * 2 // 19
-        boardsize = cellsize * 9
+        boardsize = min(self.size) * 17 // 18
+        cellsize = boardsize // 9
+        yoffset = self.width * 5 // 72
         return BoardDecoder(self,
-                            ((self.width - boardsize) // 2 + 1,
-                             (self.height - boardsize) // 2 - 53),
+                            (self.width // 2,
+                             self.height // 2 - yoffset),
                             cellsize)
 
 
 class BoardDecoder:
-    def __init__(self, img, topleft, cellsize):
+    def __init__(self, img, cen_xy, cellsize):
         self.img = img
-        self.topleft = topleft
+        self.cen_xy = cen_xy
         self.cellsize = cellsize
 
     @property
     def cells(self):
-        left, top = self.topleft
-        for row in range(9):
-            ycen = ((row * 2 + 1) * self.cellsize) // 2 + top
-            for col in range(9):
-                xcen = ((col * 2 + 1) * self.cellsize) // 2 + left
+        for row in range(-4, 5):
+            ycen = self.cen_xy[1] + row * self.cellsize
+            for col in range(-4, 5):
+                xcen = self.cen_xy[0] + col * self.cellsize
                 yield CellDecoder(self.img, (xcen, ycen), self.cellsize)
 
     def tolist(self):
